132d6bd4f8a67bbb6a57fe0143977b821b7aa19d
Pepijn de Vos <pepijndevos@gmail.com>
2012-04-04 20:05:46 +0200

    dummy gui

---
+++ dpscope/gui.py
@@ -0,0 +1,114 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from portselect import get_port
+from Tkinter import *
+
+root = Tk()
+root.title("DPScope")
+fig = Figure()
+plt = fig.add_subplot(111)
+
+# the plot
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.show()
+canvas.get_tk_widget().pack(fill=BOTH, expand=1, side=LEFT)
+
+# the controls
+controls = Frame(root)
+controls.pack(side=LEFT)
+coll1 = Frame(controls)
+coll1.pack(fill=BOTH, expand=1, side=LEFT)
+coll2 = Frame(controls)
+coll2.pack(fill=BOTH, expand=1, side=LEFT)
+
+# all control sections
+acquisition = LabelFrame(coll1, text="Acquisition")
+acquisition.pack(fill=BOTH, expand=1)
+
+levels = LabelFrame(coll1, text="Levels")
+levels.pack(fill=BOTH, expand=1)
+
+display = LabelFrame(coll2, text="Display")
+display.pack(fill=BOTH, expand=1)
+
+vertical = LabelFrame(coll2, text="Vertical")
+vertical.pack(fill=BOTH, expand=1)
+
+horizontal = LabelFrame(coll2, text="Horizontal")
+horizontal.pack(fill=BOTH, expand=1)
+
+trigger = LabelFrame(coll2, text="Trigger")
+trigger.pack(fill=BOTH, expand=1)
+
+# acquisition controls
+Button(acquisition, text="Start").pack(fill=X)
+Button(acquisition, text="Poll").pack(fill=X)
+Button(acquisition, text="Stop").pack(fill=X)
+Button(acquisition, text="Clear").pack(fill=X)
+Label(acquisition, text="Average").pack(fill=X)
+Spinbox(acquisition, from_=1, to=100, width=4).pack(fill=X)
+
+# level controls
+Label(levels, text="Ch1").grid(sticky=E, row=0, column=0)
+Label(levels, text="Ch1").grid(sticky=E, row=0, column=1)
+Label(levels, text="Trg").grid(sticky=E, row=0, column=2)
+Scale(levels, from_=-100, to=100, length=300).grid(sticky=E, row=1, column=0)
+Scale(levels, from_=-100, to=100, length=300).grid(sticky=E, row=1, column=1)
+Scale(levels, from_=-100, to=100, length=300).grid(sticky=E, row=1, column=2)
+
+# Display controls
+Checkbutton(display, text="Ch1").grid(sticky=W, row=0, column=1)
+Checkbutton(display, text="Ch2").grid(sticky=W, row=1, column=1)
+Checkbutton(display, text="X/Y").grid(sticky=W, row=2, column=1)
+Checkbutton(display, text="FFT").grid(sticky=W, row=3, column=1)
+
+# Vertical controls
+gains = ["1 V/div", "0.5 V/div", "0.2 V/div", "0.1 V/div", "50 mV/div", "20 mV/div", "10 mV/div", "5 mV/div"]
+Label(vertical, text="Scale").grid(sticky=W, row=0, column=1)
+Label(vertical, text="Probe Attenuation").grid(sticky=W, row=0, column=2, columnspan=2)
+
+Label(vertical, text="Ch1").grid(sticky=W, row=2, column=0)
+ch1att = BooleanVar()
+ch1gain = StringVar()
+ch1gain.set(gains[0])
+OptionMenu(vertical, ch1gain, *gains).grid(sticky=W, row=2, column=1)
+Radiobutton(vertical, text="1:1", variable=ch1att, value=0).grid(sticky=W, row=2, column=2)
+Radiobutton(vertical, text="1:10", variable=ch1att, value=1).grid(sticky=W, row=2, column=3)
+
+Label(vertical, text="Ch2").grid(sticky=W, row=4, column=0)
+ch2att = BooleanVar()
+ch2gain = StringVar()
+ch2gain.set(gains[0])
+OptionMenu(vertical, ch2gain, *gains).grid(sticky=W, row=4, column=1)
+Radiobutton(vertical, text="1:1", variable=ch2att, value=0).grid(sticky=W, row=4, column=2)
+Radiobutton(vertical, text="1:10", variable=ch2att, value=1).grid(sticky=W, row=4, column=3)
+
+# Horizontal controls
+speeds = ["0.5 us/div", "1 us/div", "2 us/div", "5 us/div", "10 us/div", "20 us/div", "50 us/div", "0.1 ms/div", "0.2 ms/div", "0.5 ms/div", "1 ms/div", "2 ms/div", "5 ms/div", "10 ms/div", "20 ms/div", "50 ms/div", "0.1 s/div", "0.2 s/div", "0.5 s/div", "1 s/div"]
+sample_speed = StringVar()
+sample_speed.set(speeds[0])
+samepl_mode = BooleanVar()
+Radiobutton(horizontal, text="Scope mode", variable=samepl_mode, value=0).grid(sticky=W, row=0, column=0)
+Radiobutton(horizontal, text="Datalog mode", variable=samepl_mode, value=1).grid(sticky=W, row=0, column=1)
+OptionMenu(horizontal, sample_speed, *gains).grid(sticky=W, row=1, column=0, columnspan=2)
+Checkbutton(horizontal, text="Pretrigger mode").grid(sticky=W, row=2, column=0, columnspan=2)
+Scale(horizontal, from_=0, to=100, length=200, orient=HORIZONTAL).grid(sticky=W, row=3, column=0, columnspan=2)
+
+# Trigger controls
+Label(trigger, text="Source").grid(sticky=W, row=0, column=0)
+trigger_mode = IntVar()
+Radiobutton(trigger, text="Auto", variable=trigger_mode, value=0).grid(sticky=W, row=0, column=1)
+Radiobutton(trigger, text="Ch1", variable=trigger_mode, value=1).grid(sticky=W, row=0, column=2)
+Radiobutton(trigger, text="Ch2", variable=trigger_mode, value=2).grid(sticky=W, row=0, column=3)
+
+Label(trigger, text="Polarity").grid(sticky=W, row=1, column=0)
+trigger_pol = BooleanVar()
+Radiobutton(trigger, text="Rising", variable=trigger_pol, value=0).grid(sticky=W, row=1, column=1)
+Radiobutton(trigger, text="Falling", variable=trigger_pol, value=1).grid(sticky=W, row=1, column=2)
+Checkbutton(trigger, text="Noise reject").grid(sticky=W, row=1, column=3)
+
+
+#get_port(root)
+root.mainloop()
--- dpscope/high.py
+++ dpscope/high.py
@@ -1,27 +1,14 @@
 from low import DPScope
-import matplotlib.pyplot as plt
-from serial.tools.list_ports import comports
 
-ports = comports()
+def read(nofb):
+    data = None
+    while not data:
+        data = s.read_back(nofb)
+    return data
 
-for i, p in enumerate(ports):
-    print i, p[1]
+def reader(nofb):
+   while True:
+       yield read(nofb)
 
-n = int(raw_input("Enter port number: "))
-port = ports[n][0]
-
-s = DPScope(port)
-s.cal_mode(1)
-s.trig_source(0)
-s.arm(0)
-status = 0
-while not status:
-    data = s.read_back(205)
-    status = data[0]
-s.abort()
-
-ch1 = data[2::2]
-ch2 = data[3::2]
-plt.plot(ch1)
-plt.plot(ch2)
-plt.show()
+def channels(data):
+    return data[1::2], data[2::2]
